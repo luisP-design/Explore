4bdd0822fe091576e80cfc874ac3bde4e52e61bc
Luis P <g.palomo.luis@gmail.com>
2020-12-27 16:46:54 -0300

    Making header & hero section

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,14 @@
 const hero = document.getElementById('header');
 const heroFTitle = document.getElementById('hero-place-title');
-let images = ['Dubrovnik-Ragusa', 'Santorini-Greece'];
+let images = ['Dubrovnik-Ragusa', 'Santorini-Greece', 'Hawaii-Estados-Unidos', 'California-Estados-Unidos'];
 
 function stringDivi(string) {
     let name = string.split('-');
-    return name[0] + ", " + name[1]
+    if (name.length == 2) {
+        return name[0] + ", " + name[1]
+    } else {
+        return name[0] + ", " + name[1] + " " + name[2]
+    }
 }
 
 function bgImage(imgName) {
@@ -12,7 +16,7 @@ function bgImage(imgName) {
     hero.style.backgroundImage = `url(./images/${imgName[num]}.jpg)`;
     hero.style.backgroundRepeat = "no-repeat";
     hero.style.backgroundSize = "cover";
-    hero.style.backgroundPositionY = "center"
+    hero.style.backgroundPositionY = "center";
     heroFTitle.innerHTML = `${stringDivi(imgName[num])}`;
 }
 
